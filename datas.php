ea5380ae1f8ecd5807d15240b40ce9d31dab152c
arieltonsilveira <arieltonsilveira@gmail.com>
2022-01-25 11:31:12 -0400

    desafio completo

---
+++ datas.php
@@ -0,0 +1,248 @@
+<?php
+
+define('grupo_meses_30_dias', array(11, 9, 6, 4));
+
+/**
+* Calcula o numero de dias entre 2 datas.
+* As datas passadas sempre serao validas e a primeira data sempre sera menor que a segunda data.
+* @param string $dataInicial No formato YYYY-MM-DD
+* @param string $dataFinal No formato YYYY-MM-DD
+* @return int O numero de dias entre as datas
+**/
+function calculaDias($dataInicial, $dataFinal) {
+	/*
+		- Setembro, abril, junho e novembro tem 30 dias, todos os outros meses tem 31 exceto fevereiro que tem 28, exceto nos anos bissextos nos quais ele tem 29.
+		- Os anos bissexto tem 366 dias e os demais 365.
+		- Todo ano divisivel por 4 e um ano bissexto.
+		- A regra acima não e valida para anos divisiveis por 100. Estes anos devem ser divisiveis por 400 para serem anos bissextos. Assim, o ano 1700, 1800, 1900 e 2100 nao sao bissextos, mas 2000 e bissexto.
+		- Não e permitido o uso de classes e funcoes de data da linguagem (DateTime, mktime, strtotime, etc).
+	*/
+
+	$dataInicial = explode('-', $dataInicial);
+	$dataFinal = explode('-', $dataFinal);
+
+	$diferencaDias = diferençaEntreAnosEmDias(pegarAno($dataInicial), pegarAno($dataFinal));
+	$mesFimEmDias = convertMesesEmDias(pegarMes($dataFinal), anoBissexto(pegarAno($dataFinal)));
+	$mesInicioEmDias = convertMesesEmDias(pegarMes($dataInicial), anoBissexto(pegarAno($dataInicial)));
+
+	return ($mesFimEmDias + pegarDia($dataFinal)) - ($mesInicioEmDias + pegarDia($dataInicial)) + $diferencaDias;
+}
+
+// ---------------- Conversão de Ano ----------------//
+
+function diferençaEntreAnosEmDias($anoInicio, $anoFim) {
+	$dias = 0;
+	if ($anoInicio === $anoFim) return $dias;
+
+	while ($anoInicio < $anoFim) {
+		$dias += converteAnoEmDias($anoInicio);
+		$anoInicio += 1;
+	}
+	return $dias;
+}
+
+function converteAnoEmDias($ano) {
+	if (anoBissexto($ano)) {
+		return 366;
+	}
+	return 365;
+}
+
+function anoBissexto($ano) {
+	if ($ano % 4 === 0) {
+		if (($ano % 100 === 0) && ($ano % 400 !== 0)) {
+			return false;
+		}
+		return true;
+	}
+	return false;
+}
+
+
+// ----------- Conversão de Mes --------------//
+
+function convertMesesEmDias($mes, $ano_bissexto) {
+	$dias = 0;
+	$mes -= 1;
+	while ($mes >= 1) {
+		$dias += diasNoMes($mes, $ano_bissexto);
+		$mes -= 1;
+	}
+	return $dias;
+}
+
+function diasNoMes($mes, $ano_bissexto) {
+	if ($mes == 2) {
+		if ($ano_bissexto) {
+			return 29;
+		}
+		return 28;
+	}
+	if (in_array($mes, grupo_meses_30_dias)) {
+		return 30;
+	}
+	return 31;
+}
+
+function pegarAno($data) {
+	return (int) $data[0];
+}
+
+function pegarMes($data) {
+	return (int) $data[1];
+}
+
+function pegarDia($data) {
+	return (int) $data[2];
+}
+
+/***** Teste 01 *****/
+$dataInicial = "2018-01-01";
+$dataFinal = "2018-01-02";
+$resultadoEsperado = 1;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("01", $resultadoEsperado, $resultado);
+
+/***** Teste 02 *****/
+$dataInicial = "2018-01-01";
+$dataFinal = "2018-02-01";
+$resultadoEsperado = 31;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("02", $resultadoEsperado, $resultado);
+
+/***** Teste 03 *****/
+$dataInicial = "2018-01-01";
+$dataFinal = "2018-02-02";
+$resultadoEsperado = 32;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("03", $resultadoEsperado, $resultado);
+
+/***** Teste 04 *****/
+$dataInicial = "2018-01-01";
+$dataFinal = "2018-02-28";
+$resultadoEsperado = 58;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("04", $resultadoEsperado, $resultado);
+
+/***** Teste 05 *****/
+$dataInicial = "2018-01-15";
+$dataFinal = "2018-03-15";
+$resultadoEsperado = 59;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("05", $resultadoEsperado, $resultado);
+
+/***** Teste 06 *****/
+$dataInicial = "2018-01-01";
+$dataFinal = "2019-01-01";
+$resultadoEsperado = 365;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("06", $resultadoEsperado, $resultado);
+
+/***** Teste 07 *****/
+$dataInicial = "2018-01-01";
+$dataFinal = "2020-01-01";
+$resultadoEsperado = 730;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("07", $resultadoEsperado, $resultado);
+
+/***** Teste 08 *****/
+$dataInicial = "2018-12-31";
+$dataFinal = "2019-01-01";
+$resultadoEsperado = 1;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("08", $resultadoEsperado, $resultado);
+
+/***** Teste 09 *****/
+$dataInicial = "2018-05-31";
+$dataFinal = "2018-06-01";
+$resultadoEsperado = 1;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("09", $resultadoEsperado, $resultado);
+
+/***** Teste 10 *****/
+$dataInicial = "2018-05-31";
+$dataFinal = "2019-06-01";
+$resultadoEsperado = 366;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("10", $resultadoEsperado, $resultado);
+
+/***** Teste 11 *****/
+$dataInicial = "2016-02-01";
+$dataFinal = "2016-03-01";
+$resultadoEsperado = 29;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("11", $resultadoEsperado, $resultado);
+
+/***** Teste 12 *****/
+$dataInicial = "2016-01-01";
+$dataFinal = "2016-03-01";
+$resultadoEsperado = 60;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("12", $resultadoEsperado, $resultado);
+
+/***** Teste 13 *****/
+$dataInicial = "1981-09-21";
+$dataFinal = "2009-02-12";
+$resultadoEsperado = 10006;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("13", $resultadoEsperado, $resultado);
+
+/***** Teste 14 *****/
+$dataInicial = "1981-07-31";
+$dataFinal = "2009-02-12";
+$resultadoEsperado = 10058;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("14", $resultadoEsperado, $resultado);
+
+/***** Teste 15 *****/
+$dataInicial = "2004-03-01";
+$dataFinal = "2009-02-12";
+$resultadoEsperado = 1809;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("15", $resultadoEsperado, $resultado);
+
+/***** Teste 16 *****/
+$dataInicial = "2004-03-01";
+$dataFinal = "2009-02-12";
+$resultadoEsperado = 1809;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("16", $resultadoEsperado, $resultado);
+
+/***** Teste 17 *****/
+$dataInicial = "1900-02-01";
+$dataFinal = "1900-03-01";
+$resultadoEsperado = 28;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("17", $resultadoEsperado, $resultado);
+
+/***** Teste 18 *****/
+$dataInicial = "1899-01-01";
+$dataFinal = "1901-01-01";
+$resultadoEsperado = 730;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("18", $resultadoEsperado, $resultado);
+
+/***** Teste 19 *****/
+$dataInicial = "2000-02-01";
+$dataFinal = "2000-03-01";
+$resultadoEsperado = 29;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("19", $resultadoEsperado, $resultado);
+
+/***** Teste 20 *****/
+$dataInicial = "1999-01-01";
+$dataFinal = "2001-01-01";
+$resultadoEsperado = 731;
+$resultado = calculaDias($dataInicial, $dataFinal);
+verificaResultado("20", $resultadoEsperado, $resultado);
+
+
+function verificaResultado($nTeste, $resultadoEsperado, $resultado) {
+	if(intval($resultadoEsperado) == intval($resultado)) {
+		echo "Teste $nTeste passou.\n";
+	} else {
+		echo "Teste $nTeste NAO passou (Resultado esperado = $resultadoEsperado, Resultado obtido = $resultado).\n";
+	}
+}
+
+?>
